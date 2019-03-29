a7772b3e7b6c1daa8c96ea088a3960f3ac7226e7
ferryp2 <patrick.ferry2@mail.dcu.ie>
2019-03-29 12:36:19 +0000

    .

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,21 @@
 from math import floor, log2
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+
+
+def time_coef(frame, hop_size, sr):
+    return (frame*hop_size)/sr
+
+
+def freq_coef(ind, sr=44100, win_len=8192):
+    return (ind * sr)/win_len
+
+
+def mag_to_db(mag):
+    # for i in range(len(mag)):
+    #     mag[i] = 20 * np.log10(mag[i])
+    return np.where(mag > 0, 20 * np.log10(mag), 0)
 
 
 def midi_to_pitch(note, tuning=440):
@@ -28,8 +44,6 @@ def log_compression(spect, y=1):
 def bucket_size():
     # TODO: Maybe try remove this
     for i in np.arange(11.5, 108.5, 1):  # Maybe change 11.5 to zero?
-        # print(midi_to_pitch(i+.5))
-        # print(i+.5)
         yield int(i+.5), midi_to_pitch(i), midi_to_pitch(i+1)
 
 
@@ -46,18 +60,109 @@ def log_freq_spec(spect):
     return new_full
 
 
-def freq_to_bucket(freq, cents=100):
-    return floor((1200/cents)*log2(freq/440)+69.5)
+def freq_to_bucket(freq, cents=100, ref=8.66):
+    freq = freq_coef(freq)
+    return floor((1200/cents)*log2(freq/ref)+1.5)
 
 
-def refined_log_freq_spec(spect):
+def display(spec, text="STFT"):
+    plt.pcolormesh(np.abs(spec))
+    plt.colorbar()
+    plt.title(text)
+    plt.ylabel('Frequency ')
+    plt.xlabel('Time ')
+    plt.show()
+
+
+def refined_log_freq_spec(spect, bins=128):
     # TODO: This function breaks if freq_to_buckets cents is set to anything small as it tries to get negative index
     song_length = len(spect[0])
-    freq_range = len(spect)
-    # print(spect.shape)
-    new_full = np.zeros(shape=(freq_to_bucket(freq_range-1), song_length))
-    # print(new_full.shape)
+    new_full = np.zeros(shape=(bins, song_length), dtype='complex')
     for index in range(1, len(spect)):
-        new_full[freq_to_bucket(index)-1] = new_full[freq_to_bucket(index)-1] + spect[index]
+        print(index)
+        if freq_to_bucket(index) >= 128:
+            break
+        # TODO: When peak picking has been fixed look into this
+        new_full[freq_to_bucket(index)] += spect[index]
         # print(freq_to_bucket(index))
     return new_full
+
+
+def select_peaks(frame):
+    """Takes a frame of stft and only keeps the peaks"""
+    normal = np.abs(frame)
+    peaks, _ = signal.find_peaks(normal)
+    mask = np.zeros_like(frame, dtype='complex')
+    for peak in peaks:
+        mask[peak] = 1
+    return peaks, frame * mask
+
+
+def magphase(spect):
+    mag = np.abs(spect)
+    phase = np.exp(1.j * np.angle(spect))
+    return mag, phase
+
+
+def harmonic_summ(arr):
+    # TODO: This is not changing the output at all
+    # TODO: IMPORTANT
+    for i in range(len(arr)-28):
+        arr[i] += arr[i+12] + arr[i+19] + arr[i+24] + arr[i+28]
+    return arr
+
+
+def my_stft(song):
+    # TODO: Tidy this up
+    win_len = 2048
+    w = signal.get_window("hann", win_len)
+    hop_size = 128
+    short = np.zeros(shape=(4096, (len(song) - win_len) // hop_size), dtype=complex)
+    totes = np.ones(shape=w.shape)
+    for i in range(0, len(song) - win_len, hop_size):
+        # TODO: Fix the normalisation peak picking
+        # TODO: Threshold here is currently just average, find a better solution
+        # Phase is not being added to the stft
+        windowed = song[i:i + win_len] * w
+        this = np.fft.fft(windowed, n=8192)
+        this = this[:len(this) // 2]
+        short[:, (i // hop_size) - 1] = this
+    return short
+
+
+def voicing(t, threshold=1):
+    """Takes a column of stft and a volume threshold and will return any notes that are present"""
+    if np.max(t) > 1000000:
+        return np.argmax(t)
+    else:
+        return -1
+
+
+def bin_offset(k, kval, kval2, N =8192, sr=44100, H=128):
+    first = N/H
+    second = np.angle(kval - kval2 - ((k*H)/N))/(np.pi*2)
+    return k + (first*second)*(sr/N)
+
+
+def refined_phase(spec):
+    pass
+
+
+def phase_correct(spec, N=8192, H=128):
+    first = N / (np.pi*H)
+    for i in range(1, len(spec[0])-1):
+        curr = spec[:, i]
+        prev = spec[:, i-1].imag
+        icurr = curr.imag
+        principal = np.angle(icurr - prev - ((np.pi*H)/N)*curr)
+        spec[:, i] += (first * principal)
+        print("curr", curr)
+        print(curr + (first * principal), "\n")
+    return spec
+
+
+def harmsumm(frame, peaks):
+    for peak in peaks:
+        if peak < 100:
+            frame[peak] += frame[peak + 12] + frame[peak + 19] + frame[peak + 24] + frame[peak + 28]
+    return frame
