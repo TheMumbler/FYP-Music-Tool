862bf1825d8c2ae66abbed46e228cb708f1a3d84
ferryp2 <patrick.ferry2@mail.dcu.ie>
2019-05-18 13:39:00 +0100

    added evaluation file

--- src/test/evaluate.py
+++ src/test/evaluate.py
@@ -1,4 +1,5 @@
 import py_midicsv
+import csv
 
 
 def convert_to_csv(file, output_name):
@@ -13,3 +14,54 @@ def convert_to_csv(file, output_name):
     with open(output_name, 'w') as file:
         for line in csv_string:
             file.write(line)
+
+
+oracle = {}
+result = {}
+
+with open('tidied.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            pass
+            line_count += 1
+        else:
+            oracle[row[0]] = row[1]
+            # print(f'\t{row[0]} works in the {row[1]})
+            line_count += 1
+
+
+with open('tidiedR.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            pass
+            line_count += 1
+        else:
+            result[row[0]] = int(row[1])+12
+            # print(f'\t{row[0]} works in the {row[1]})
+            line_count += 1
+
+match = 0
+error = 50
+for key, item in oracle.items():
+    for i in range(int(key)-error, int(key)+error):
+        check = result.get(str(i), None)
+        if check:
+            # print((check), type(item))
+            if int(check) == int(item):
+                print("MATCH")
+                match += 1
+                # continue
+
+recall = match/len(oracle)
+precision = match/len(result)
+fscore = 2*((precision*recall)/(precision+recall))
+
+print("Recall:", recall)
+print("Precision:", precision)
+print("F1 Score:", fscore)
+
+
